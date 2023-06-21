ea2a1bd671e03cf212d47d934ea4a592fb8c49c6
tonijigan <100856397+tonijigan@users.noreply.github.com>
2023-06-21 13:28:00 +0300

    Добавьте файлы проекта.

---
+++ NapilnikLoger/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace NapilnikLoger
+{
+    interface ILogger
+    {
+        void WriteError(string message);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pathfinder pathfinder = new Pathfinder(new FileLogWritter());
+            pathfinder.WriteError("s");
+        }
+    }
+
+    class ConsoleLogWritter : ILogger
+    {
+        public virtual void WriteError(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class FileLogWritter : ILogger
+    {
+        public virtual void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);
+        }
+    }
+
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+
+    class Pathfinder : ILogger
+    {
+        protected ILogger _logger;
+
+        public Pathfinder(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void WriteError(string message)
+        {
+
+        }
+    }
+}
